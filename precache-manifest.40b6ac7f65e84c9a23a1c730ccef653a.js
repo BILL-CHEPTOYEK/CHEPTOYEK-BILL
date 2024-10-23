f560ff84cf8dc2ca40dd3beff2d71b28ac710a75
BILL-CHEPTOYEK <BILL-CHEPTOYEK@users.noreply.github.com>
2024-10-23 15:17:34 +0000

    deploy: 9acc9afcd9486b9d378a86826930d51c333f18da

---
+++ precache-manifest.40b6ac7f65e84c9a23a1c730ccef653a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6330e1b2842e4effc7fc245dad7f13",
+    "url": "/CHEPTOYEK-BILL/index.html"
+  },
+  {
+    "revision": "2fc7696f9337d53ab18f",
+    "url": "/CHEPTOYEK-BILL/static/css/main.122a9532.chunk.css"
+  },
+  {
+    "revision": "3092b6a32cf7e376efaf",
+    "url": "/CHEPTOYEK-BILL/static/js/2.49a4ccfc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/CHEPTOYEK-BILL/static/js/2.49a4ccfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc7696f9337d53ab18f",
+    "url": "/CHEPTOYEK-BILL/static/js/main.0bda2816.chunk.js"
+  },
+  {
+    "revision": "71c2058de1edeb284ed7",
+    "url": "/CHEPTOYEK-BILL/static/js/runtime-main.c00b634a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.853f5cc096be025163ec35c78648a390.js"
+  "/CHEPTOYEK-BILL/precache-manifest.40b6ac7f65e84c9a23a1c730ccef653a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CHEPTOYEK-BILL/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
