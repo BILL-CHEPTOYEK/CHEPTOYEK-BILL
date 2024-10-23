48e136278d5f750e32f77208f54c9c05219f787d
Bill Cheptoyek <108127528+BILL-CHEPTOYEK@users.noreply.github.com>
2024-10-23 17:19:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 import App from './App';
+
+// Use ReactDOM to render App
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
