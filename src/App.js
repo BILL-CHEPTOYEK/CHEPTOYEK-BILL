41997d3a1b53c7d854b2f36e5ae35d4b9512c74c
Bill <billcheptoyek60@gmail.com>
2025-07-27 23:53:59 +0300

    updated-cv

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
             <li>DevOps</li>
             <li>Git & GitHub</li>
             <li>REST APIs</li>
-            <li>Agile & Sc</li>
+            <li>Agile & S</li>
           </ul>
         </div>
       </section>
