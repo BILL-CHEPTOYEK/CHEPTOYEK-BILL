a7522341a66ea1b2846b1c583aa3c2026b3a0ef2
Bill <billcheptoyek60@gmail.com>
2025-08-08 17:25:10 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
           and user experience.
         </p>
         <p>
-          My toolkit includes React, Node.js, Python, Flutter, AI, ML, and blockchai
+          My toolkit includes React, Node.js, Python, Flutter, AI, ML, and blockcha
         </p>
       </section>
 
