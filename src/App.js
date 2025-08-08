33084edb2dbd6d7d3f47eca9ddf7e5897e1bfbfe
Bill <billcheptoyek60@gmail.com>
2025-08-08 17:04:14 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function App() {
             description="Comprehensive RESTful API for hotel operations using Node.js and MongoDB."
             github="https://github.com/BILL-CHEPTOYEK/hotelms-api"
           />
-          {/* Add other projects s */}
+          {/* Add other projects  */}
         </div>
       </section>
 
