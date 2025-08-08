8bfb4633aa4263907e91add49a5e2038d767f304
Bill <billcheptoyek60@gmail.com>
2025-08-08 17:22:24 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
           and user experience.
         </p>
         <p>
-          My toolkit includes React, Node.js, Python, Flutter, AI, ML, and blockchain. I thriv
+          My toolkit includes React, Node.js, Python, Flutter, AI, ML, and blockchain. I thri
         </p
       </section>
 
