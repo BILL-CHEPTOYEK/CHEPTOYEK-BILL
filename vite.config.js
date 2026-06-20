b74cb3ef1d5693db75b572d0140e9584f401dd70
Bill Cheptoyek <108127528+BILL-CHEPTOYEK@users.noreply.github.com>
2026-06-20 12:11:35 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CHEPTOYEK-BILL/',
+  base: '/',
   plugins: [
     react({
       babel: {
