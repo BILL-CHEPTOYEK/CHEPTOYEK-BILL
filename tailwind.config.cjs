9a3be99945c575508c1c34c3dfe7f53d934f537d
Bill Cheptoyek <108127528+BILL-CHEPTOYEK@users.noreply.github.com>
2026-06-30 11:09:20 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+  extend: {
+    fontFamily: {
+      heathergreen: ["Heathergreen", "sans-serif"],
+    },
   },
+}
   plugins: [],
 }
