a4045bf258d92aedf6e300451c146be08feb1446
Bill Cheptoyek <108127528+BILL-CHEPTOYEK@users.noreply.github.com>
2026-07-06 15:00:39 +0300

    Update CitizenHomePage.jsx

--- src/pages/CitizenHomePage.jsx
+++ src/pages/CitizenHomePage.jsx
@@ -11,7 +11,7 @@ export default function CitizenHomePage() {
         {/* Identity */}
         <div className="space-y-6">
 
-          <div text-center>
+          <div className="text-center">
             <h1 className="text-4xl md:text-5xl font-semibold tracking-tight">
               CHEPTOYEK BILL
             </h1>
